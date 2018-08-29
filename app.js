b2414d98a77ce817bc4de5a7b3bf0ae4fc0ea674
wongprom <jimmybackstrom@hotmail.com>
2018-08-29 09:52:32 +0200

    Calculate numbers and output to UI

--- app.js
+++ app.js
@@ -17,5 +17,20 @@ function calculateResults(e) {
   const calculatedInterest = parseFloat(ELinterest.value) / 100 / 12;
   const calculatedPayments = parseFloat(ELyears.value) * 12;
 
+  //Calculate monthly payment
+  //math.pow(4, 3) innebar "upphojt i" 4*4*4
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+  //isFinite() checks if number is infinite
+  if (isFinite(monthly)) {
+    ELmonthlyPayment.value = monthly.toFixed(2);
+    ELtotalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    ELtotalInterest.value = (monthly * calculatedPayments - principal).toFixed(
+      2
+    );
+  } else {
+    console.log('Pleas check your number input..');
+  }
+
   e.preventDefault();
 }
